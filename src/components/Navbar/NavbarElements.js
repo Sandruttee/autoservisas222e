88a1c9c7be39d132cb2c880979c5ff3c0fdfbdd9
Sandruttee <sandra.placenyte@gmail.com>
2023-10-16 00:26:22 +0200

    added content

--- src/components/Navbar/NavbarElements.js
+++ src/components/Navbar/NavbarElements.js
@@ -1,13 +1,12 @@
 import { NavLink as Link } from "react-router-dom";
 import styled from "styled-components";
-import "./NavbarElements.css";
 
 export const Nav = styled.nav`
   background: #000000;
   height: 85px;
   display: flex;
   justify-content: space-between;
-  padding: 0.2rem calc((100vw - 1000px) / 2);
+  padding: 0.2rem calc((130vw - 1000px) / 2);
   z-index: 12;
 `;
 
--- src/pages/Autoservisas.js
+++ src/pages/Autoservisas.js
@@ -4,8 +4,31 @@ import "./styling.css";
 
 const Autoservisas = () => {
   return (
-    <div class="container">
-      <img src={logo} class="img-fluid autoservisas-logo" alt="logo" />
+    <div class="page-container">
+      <div class="container">
+        <img src={logo} class="img-fluid autoservisas-logo" alt="logo" />
+      </div>
+      <p>
+        <h1>Sveiki atvykę į AUTOSERVISAS 222e</h1>
+        <h2>Mes rūpinamės Jūsų automobiliais nuo 2021-ųjų</h2>
+        <h3>Mes atliekame šias paslaugas: </h3>
+        <ul>
+          <li>Važiuoklės remontas</li>
+          <li>Tepalų ir filtrų keitimas</li>
+          <li>Stabdžių sistemos remontas</li>
+          <li>Kompiuterinė diagnostika</li>
+          <li>Sankabos remontas</li>
+          <li>Kondicionierių pildymas ir remontas</li>
+          <li>Kitos paslaugos</li>
+        </ul>
+      </p>
+      <footer>
+        <h4>
+          AUTO<span class="red-text">SERVISAS 222E</span>
+        </h4>
+        <p class="p-footer">Mus rasite adresu: Staniūnų g. 67a, Panevėžys</p>
+        <p class="p-footer">Susisiekite su mumis: +37063222439</p>
+      </footer>
     </div>
   );
 };
