2a6d69478ef5c9c62461c3308cfe996c2d67bcd7
Sandruttee <sandra.placenyte@gmail.com>
2023-10-15 22:11:23 +0200

    created a nav bar

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
 import "./App.css";
+import Page from "./Page";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header"></header>
+      <Page />
     </div>
   );
 }
