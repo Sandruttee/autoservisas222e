9dfe454e613b5e41e0112f94691a73e3a2611857
Sandruttee <sandra.placenyte@gmail.com>
2023-11-04 12:47:15 +0100

    login build

--- src/pages/Booking.js
+++ src/pages/Booking.js
@@ -1,8 +1,41 @@
-import React from "react";
+import React, { useState } from "react";
 
 const Booking = () => {
+  const [username, setUsername] = useState(null);
+  const [password, setPassword] = useState(null);
+  function handleSubmit(event) {
+    event.preventDefault();
+    if (username === "x" && password === "y") {
+      alert("hi");
+    }
+  }
   return (
     <div class="page-container">
+      <main className="login">
+        <form className="login__form" onSubmit={handleSubmit}>
+          <h5 className="login__title">Prisijunkite prie savo paskyros</h5>
+          <label htmlFor="username">Vartotojo vardas</label>
+          <input
+            id="username"
+            name="username"
+            type="text"
+            value={username}
+            onChange={setUsername}
+            className="username"
+          />
+          <label htmlFor="password">Slaptažodis</label>
+          <input
+            id="password"
+            type="password"
+            name="password"
+            value={password}
+            onChange={setPassword}
+            className="password"
+          />
+          <input className="loginButton" type="submit" value="Prisijungti" />
+        </form>
+      </main>
+
       <footer>
         <h4>
           AUTO<span class="red-text">SERVISAS 222E</span>
