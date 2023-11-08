b05b418008fd65e4a36226373dbd952142b34f40
Sandruttee <sandra.placenyte@gmail.com>
2023-11-08 12:54:43 +0100

    added a footer to user page

--- src/pages/User.js
+++ src/pages/User.js
@@ -1,7 +1,18 @@
 import React from "react";
 
-const Secret = () => {
-  return <div>hello world</div>;
+const User = () => {
+  return (
+    <div>
+      {" "}
+      <footer>
+        <h4>
+          AUTO<span class="red-text">SERVISAS 222E</span>
+        </h4>
+        <p class="p-footer">Mus rasite adresu: Staniūnų g. 67a, Panevėžys</p>
+        <p class="p-footer">Susisiekite su mumis: +37063222439</p>
+      </footer>
+    </div>
+  );
 };
 
-export default Secret;
+export default User;
