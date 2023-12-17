a59aedf95487eafa0e3e150051fe8c49c22f6852
Sandruttee <sandra.placenyte@gmail.com>
2023-12-17 18:12:17 +0100

    bug fix

--- src/pages/AvailableTimeForm.js
+++ src/pages/AvailableTimeForm.js
@@ -1,6 +1,6 @@
 import React from "react";
 import { useDispatch, useSelector } from "react-redux";
-import { addTime } from "./actions";
+import { addTime, setTimes } from "./actions"; // Import the setTimes action
 
 const AvailableTimeForm = () => {
   const dispatch = useDispatch();
@@ -18,68 +18,59 @@ const AvailableTimeForm = () => {
     }
   };
 
-
-  removeTime = (index) => {
-    this.setState((prevState) => {
-      const newTimes = [...prevState.times];
-      newTimes.splice(index, 1);
-      return {
-        times: newTimes,
-      };
-    });
+  const removeTime = (index) => {
+    const newTimes = [...times];
+    newTimes.splice(index, 1);
+    dispatch(setTimes(newTimes));
   };
 
-  render() {
-    const { newTime, times } = this.state;
+  return (
+    <div>
+      <div className="formContainer">
+        <h1 className="special-margin">
+          Įveskite savo laiką, kuriuo esate laisvas. Pavyzdys: 10:00-11:00
+        </h1>
+        <input
+          type="text"
+          value={newTime}
+          onChange={this.handleInputChange}
+          className="loginInput"
+        />
 
-    return (
-      <div>
-        <div className="formContainer">
-          <h1 className="special-margin">
-            Įveskite savo laiką, kuriuo esate laisvas. Pavyzdys: 10:00-11:00
-          </h1>
-          <input
-            type="text"
-            value={newTime}
-            onChange={this.handleInputChange}
-            className="loginInput"
-          />
+        <button className="loginButton" onClick={addTimeHandler}>
+          Pridėti laiką
+        </button>
 
-          <button className="loginButton" onClick={addTimeHandler}>
-            Pridėti laiką
-          </button>
-
-          <div>
-            <h3 className="special-margin">Jūs pridėjote šiuos laikus:</h3>
-            <ul>
-              {times.map((time, index) => (
-                <button
-                  className="addedTimeButton"
-                  onClick={() => this.removeTime(index)}
-                  key={index}
-                >
-                  <li>{time}</li>
-                </button>
-              ))}
-            </ul>
-            <h3 className="special-margin">
-              Norėdami pašalinti klaidingai pridėtą laiką tiesiog paspauskite
-              ant jo.
-            </h3>
-          </div>
+        <div>
+          <h3 className="special-margin">Jūs pridėjote šiuos laikus:</h3>
+          <ul>
+            {times.map((time, index) => (
+              <button
+                className="addedTimeButton"
+                onClick={() => this.removeTime(index)}
+                key={index}
+              >
+                <li>{time}</li>
+              </button>
+            ))}
+          </ul>
+          <h3 className="special-margin">
+            Norėdami pašalinti klaidingai pridėtą laiką tiesiog paspauskite ant
+            jo.
+          </h3>
         </div>
-        <footer>
-          <h4>
-            AUTO<span className="red-text">SERVISAS 222E</span>
-          </h4>
-          <p className="p-footer">
-            Mus rasite adresu: Staniūnų g. 67a, Panevėžys
-          </p>
-          <p className="p-footer">Susisiekite su mumis: +37063222439</p>
-        </footer>
       </div>
-    );
-  }
-}
+      <footer>
+        <h4>
+          AUTO<span className="red-text">SERVISAS 222E</span>
+        </h4>
+        <p className="p-footer">
+          Mus rasite adresu: Staniūnų g. 67a, Panevėžys
+        </p>
+        <p className="p-footer">Susisiekite su mumis: +37063222439</p>
+      </footer>
+    </div>
+  );
+};
 
 export default AvailableTimeForm;
