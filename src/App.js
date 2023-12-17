79d3b849f15ddf13c8cf7da3ee93159149e1c3c9
Sandruttee <sandra.placenyte@gmail.com>
2023-12-17 18:56:59 +0100

    redux install

--- src/App.js
+++ src/App.js
@@ -7,21 +7,28 @@ import Booking from "./pages/Booking";
 import Contact from "./pages/Contact";
 import BookingApp from "./pages/BookingApp";
 import AvailableTimeForm from "./pages/AvailableTimeForm";
+import { Provider } from "react-redux";
+import { createStore } from "redux";
+import rootReducer from "./pages/reducer";
+
+const store = createStore(rootReducer);
 
 function App() {
   return (
-    <div className="font">
-      <Router>
-        <Navbar />
-        <Routes>
-          <Route path="/" exact element={<Autoservisas />} />
-          <Route path="/Booking" element={<Booking />} />
-          <Route path="/Contact" element={<Contact />} />
-          <Route path="/BookingApp" element={<BookingApp />} />
-          <Route path="/AvailableTimeForm" element={<AvailableTimeForm />} />
-        </Routes>
-      </Router>{" "}
-    </div>
+    <Provider store={store}>
+      <div className="font">
+        <Router>
+          <Navbar />
+          <Routes>
+            <Route path="/" exact element={<Autoservisas />} />
+            <Route path="/Booking" element={<Booking />} />
+            <Route path="/Contact" element={<Contact />} />
+            <Route path="/BookingApp" element={<BookingApp />} />
+            <Route path="/AvailableTimeForm" element={<AvailableTimeForm />} />
+          </Routes>
+        </Router>{" "}
+      </div>
+    </Provider>
   );
 }
 
